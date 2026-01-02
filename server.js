66edcf348e49d4d17bcc54b2d878467e247e58e4
Dynamic_Krishna <leatherfoxofficail45@gmail.com>
2026-01-02 22:53:05 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const nodemon = require("nodemon");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// setInterval(() => {
+//     const now = new Date();
+//     console.log(`${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`);
+// }, 20000);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
